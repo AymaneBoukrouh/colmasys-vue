899d8e41b195c581e169969b167a255858199944
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-05-16 22:34:28 +0100

    update routes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '@/App.vue'
 import Login from '@/views/Login.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import Students from '@/views/student/Students.vue'
+import AddStudent from '@/views/student/AddStudent.vue'
+import '/src/assets/js/bootstrap.js'
 
 const routes = [
-    { path: '/login', component: Login }
+    { path: '/', component: Login },
+    { path: '/login', component: Login },
+    { path: '/dashboard', component: Dashboard },
+    { path: '/students', component: Students },
+    { path: '/students/new', component: AddStudent }
 ];
 
 const router = createRouter({
